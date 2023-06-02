57f41efafa9309c3f70d232700c0f740f8465558
deo112 <dawidjanowski@interia.pl>
2023-06-02 22:32:24 +0200

    Change directory

--- Licenses.sql
+++
@@ -1,9 +0,0 @@
-DECLARE @prawotramwaj BIT = ROUND(RAND(),0)
-DECLARE @prawoautobus BIT = ROUND(RAND(),0)
-
-WHILE (@prawotramwaj = 0 AND @prawoautobus = 0)
-BEGIN
-    SET @prawotramwaj = ROUND(RAND(),0)
-	SET @prawoautobus = ROUND(RAND(),0)
-END
-SELECT @prawotramwaj, @prawoautobus
